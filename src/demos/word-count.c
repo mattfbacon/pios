447326991807163651fb46fe0206d6b05bfb4d07
Matt Fellenz <matt@felle.nz>
2023-02-14 00:52:37 -0800

    Enhance word count demo
    Now it prints a nice prompt, and properly handles line feeds/carriage returns (in the current system, which I may change later; ideally I'd like it to be CRLF).

--- src/demos/word-count.c
+++ src/demos/word-count.c
@@ -1,10 +1,16 @@
+#include "gpio.h"
 #include "string.h"
 #include "uart.h"
 
 void main(void) {
 	uart_init();
 
+	gpio_set_mode(42, gpio_mode_output);
+	gpio_write(42, true);
+
 	while (true) {
+		uart_send_str("Enter a line: ");
+
 		u64 count = 0;
 
 		while (uart_recv() != '\n') {
@@ -17,6 +23,6 @@ void main(void) {
 			u64_to_str(buf, count);
 			uart_send_str(buf);
 		}
-		uart_send_str(" bytes.\n");
+		uart_send_str(" bytes.\r\n");
 	}
 }
