4b39c981c835e68f25ec5f2c380c113d56fd4892
Matt Fellenz <matt@felle.nz>
2023-02-16 22:55:38 -0800

    Implement driver for AHT20 sensor

---
+++ src/demos/aht20.c
@@ -0,0 +1,31 @@
+#include "devices/aht20.h"
+#include "i2c.h"
+#include "init.h"
+#include "sleep.h"
+#include "uart.h"
+
+static void print_sensor_data(aht20_data_t const data) {
+	uart_printf("the temperature is about %u °C and the humidity is about %u%%\r\n", (u32)data.temperature, (u32)data.humidity);
+}
+
+void main(void) {
+	standard_init();
+
+	uart_send_str("initializing i2c\r\n");
+	i2c_init(i2c_speed_standard);
+
+	// wait for sensor to initialize
+	sleep_micros(100'000);
+
+	uart_send_str("initializing aht20\r\n");
+	aht20_init();
+
+	sleep_micros(10'000);
+
+	while (true) {
+		uart_send_str("starting measuring\r\n");
+		aht20_data_t const data = aht20_measure();
+		print_sensor_data(data);
+		sleep_micros(5'000'000);
+	}
+}
---
+++ src/devices/aht20.c
@@ -0,0 +1,89 @@
+#include "devices/aht20.h"
+#include "i2c.h"
+#include "init.h"
+#include "sleep.h"
+#include "string.h"
+#include "uart.h"
+
+enum {
+	SENSOR_ADDRESS = 0x38,
+
+	COMMAND_CALIBRATE = 0xe1,
+	COMMAND_MEASURE = 0xac,
+	COMMAND_SOFT_RESET = 0xba,
+
+	STATUS_BUSY = 0x80,
+	STATUS_CALIBRATED = 0x08,
+};
+
+static void fail(char const* const why) {
+	uart_send_str("failed: ");
+	uart_send_str(why);
+	uart_send_str("\r\n");
+	while (true) {
+		asm volatile("wfe");
+	}
+}
+
+static void assert(bool const cond, char const* const msg) {
+	if (!cond) {
+		fail(msg);
+	}
+}
+
+static bool send(u8 const* const buf, u32 const len) {
+	return i2c_send(SENSOR_ADDRESS, buf, len);
+}
+
+static bool recv(u8* const buf, u32 const len) {
+	return i2c_recv(SENSOR_ADDRESS, buf, len);
+}
+
+static u8 get_status(void) {
+	u8 ret;
+	assert(recv(&ret, 1), "receive status");
+	return ret;
+}
+
+static void wait_until_not_busy(void) {
+	while (get_status() & STATUS_BUSY) {
+		sleep_micros(10'000);
+	}
+}
+
+static aht20_data_t decode_sensor_data(u8 const data[6]) {
+	static f32 const humidity_factor = 9.5367431640625e-05f;
+	static f32 const temperature_factor = 0.00019073486328125f;
+
+	aht20_data_t ret;
+
+	u32 const humidity_raw = (data[1] << 12) | (data[2] << 4) | (data[3] >> 4);
+	ret.humidity = (f32)humidity_raw * humidity_factor;
+
+	u32 const temperature_raw = ((data[3] & 0xf) << 16) | (data[4] << 8) | data[5];
+	ret.temperature = (f32)temperature_raw * temperature_factor - 50.0f;
+
+	return ret;
+}
+
+void aht20_init() {
+	u8 buf = 0x71;
+	assert(send(&buf, 1), "send 0x71");
+	assert((get_status() & 0x18) == 0x18, "status is not 0x18");
+}
+
+aht20_data_t aht20_measure() {
+	u8 buf[7];
+
+	buf[0] = COMMAND_MEASURE;
+	buf[1] = 0x33;
+	buf[2] = 0x00;
+	assert(send(buf, 3), "send measure command");
+
+	sleep_micros(80'000);
+	wait_until_not_busy();
+
+	assert(recv(buf, 7), "receive measurement data");
+
+	return decode_sensor_data(buf);
+}
