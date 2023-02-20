834665386409ebe64d31c93601c08539e1367f81
Matt Fellenz <matt@felle.nz>
2023-02-20 12:41:10 -0800

    Reimplement MCP23017 driver
    This implementation saves the state of various registers to significantly improve performance of common functions.

--- include/devices/mcp23017.h
+++ include/devices/mcp23017.h
@@ -4,32 +4,17 @@
 
 typedef u8 mcp23017_pin_t;
 
-enum {
-	MCP23017_ADDRESS = 0x20,
-};
-
-// returns true on success, false on failure.
-bool mcp23017_init(i2c_address_t address);
+typedef struct mcp23017_device {
+	u8 modes[2];
+	u8 pulls[2];
+	u8 outputs[2];
+	i2c_address_t address;
+} mcp23017_device_t;
 
 typedef enum mcp23017_mode {
 	mcp23017_mode_input = 1,
 	mcp23017_mode_output = 0,
 } mcp23017_mode_t;
-
-// returns true on success, false on failure.
-bool mcp23017_set_mode(i2c_address_t address, mcp23017_pin_t pin, mcp23017_mode_t mode);
-
-// returns true on success, false on failure.
-bool mcp23017_write(i2c_address_t address, mcp23017_pin_t pin, bool value);
-
-// returns true on success, false on failure.
-bool mcp23017_write_all(i2c_address_t address, u16 value);
-
-// returns true on success, false on failure.
-bool mcp23017_read(i2c_address_t address, mcp23017_pin_t pin, bool* ret);
-
-// returns true on success, false on failure.
-bool mcp23017_read_all(i2c_address_t address, u16* ret);
 
 // the MCP23017 only supports pull-up.
 typedef enum mcp23017_pull {
@@ -37,5 +22,23 @@ typedef enum mcp23017_pull {
 	mcp23017_pull_up = 1,
 } mcp23017_pull_t;
 
+enum {
+	MCP23017_ADDRESS = 0x20,
+};
+
+void mcp23017_init(mcp23017_device_t* this, i2c_address_t address);
+
 // returns true on success, false on failure.
-bool mcp23017_set_pull(i2c_address_t address, mcp23017_pin_t pin, mcp23017_pull_t pull);
+bool mcp23017_set_mode(mcp23017_device_t* this, mcp23017_pin_t pin, mcp23017_mode_t mode);
+
+// returns true on success, false on failure.
+bool mcp23017_write(mcp23017_device_t* this, mcp23017_pin_t pin, bool value);
+
+// returns true on success, false on failure.
+bool mcp23017_read(mcp23017_device_t* this, mcp23017_pin_t pin, bool* ret);
+
+// returns true on success, false on failure.
+bool mcp23017_read_all(mcp23017_device_t* this, u16* ret);
+
+// returns true on success, false on failure.
+bool mcp23017_set_pull(mcp23017_device_t* this, mcp23017_pin_t pin, mcp23017_pull_t pull);
--- src/devices/mcp23017.c
+++ src/devices/mcp23017.c
@@ -2,9 +2,6 @@
 #include "sleep.h"
 #include "uart.h"
 
-// Unfortunately, for almost all the registers here, we have to get them before we can set them, since the MCP23017 doesn't use set/clear registers like the
-// BCM2711 GPIO. However, the overhead of the double transfer is fairly trivial.
-
 enum {
 	REG_PIN_MODE = 0x00,
 	REG_POLARITY = 0x02,
@@ -12,10 +9,6 @@ enum {
 	REG_GPIO = 0x12,
 	REG_OUTPUT_LATCH = 0x14,
 };
-
-bool mcp23017_init(i2c_address_t) {
-	return true;
-}
 
 // 8 pins per port
 
@@ -27,82 +20,90 @@ static u8 shift_for_pin(mcp23017_pin_t const pin) {
 	return pin % 8;
 }
 
-#define TRY(_inner) \
-	if (!(_inner)) { \
-		return false; \
-	}
-
 // This delay was determined from testing.
 // I'm not sure if there is any official information to this end in the datasheet.
 static void io_delay(void) {
 	sleep_micros(250);
 }
 
-static bool register_read(i2c_address_t const address, u8 const reg, u8* const ret) {
-	io_delay();
+#define TRY(_inner) \
+	if (!(_inner)) { \
+		return false; \
+	}
+
+static bool read_u8(i2c_address_t const address, u8 const reg, u8* const value) {
 	TRY(i2c_send(address, &reg, 1))
-	TRY(i2c_recv(address, ret, 1))
-	return true;
+	io_delay();
+	return i2c_recv(address, value, 1);
 }
 
-static bool register_write(i2c_address_t const address, u8 const reg, u8 const value) {
+static bool read_u16(i2c_address_t const address, u8 const reg, u16* const value) {
+	TRY(i2c_send(address, &reg, 1))
 	io_delay();
+	return i2c_recv(address, (u8*)value, sizeof(*value));
+}
+
+static bool write_u8(i2c_address_t const address, u8 const reg, u8 const value) {
 	u8 const buf[2] = { reg, value };
 	return i2c_send(address, buf, sizeof(buf));
 }
 
-static bool register_set_bit(i2c_address_t const address, u8 const in_reg, u8 const out_reg, u8 const bit, bool const set) {
-	u8 reg_value;
-	TRY(register_read(address, in_reg, &reg_value))
+static bool write_reg(i2c_address_t const address, u8 const reg_base, u8* const storage_base, mcp23017_pin_t const pin, bool const value) {
+	u8 const offset = offset_for_pin(pin);
+	u8 const shift = shift_for_pin(pin);
 
-	u8 const mask = 1 << bit;
-	reg_value &= ~mask;
-	reg_value |= (u8)set << bit;
-
-	TRY(register_write(address, out_reg, reg_value))
-
-	return true;
+	u8* reg = storage_base + offset;
+	u8 new_value = *reg;
+	new_value &= ~(1 << shift);
+	new_value |= (u8)value << shift;
+	if (new_value == *reg) {
+		return true;
+	}
+	*reg = new_value;
+	return write_u8(address, reg_base + offset, *reg);
 }
 
-static bool register_set_pin_bit(i2c_address_t const address, u8 const in_reg, u8 const out_reg, mcp23017_pin_t const pin, bool const bit) {
-	u8 const pin_offset = offset_for_pin(pin);
-	return register_set_bit(address, in_reg + pin_offset, out_reg + pin_offset, shift_for_pin(pin), bit);
-}
+static bool read_reg(i2c_address_t const address, u8 const reg_base, mcp23017_pin_t const pin, bool* const ret) {
+	u8 const offset = offset_for_pin(pin);
+	u8 const shift = shift_for_pin(pin);
 
-static bool register_get_pin_bit(i2c_address_t const address, u8 const reg_offset, mcp23017_pin_t const pin, bool* const ret) {
 	u8 value;
-	TRY(register_read(address, reg_offset + offset_for_pin(pin), &value))
-	*ret = value & (1 << shift_for_pin(pin));
+	TRY(read_u8(address, reg_base + offset, &value))
+	*ret = value & (1 << shift);
 	return true;
 }
 
-bool mcp23017_set_mode(i2c_address_t const address, mcp23017_pin_t const pin, mcp23017_mode_t const mode) {
-	return register_set_pin_bit(address, REG_PIN_MODE, REG_PIN_MODE, pin, (bool)mode);
+void mcp23017_init(mcp23017_device_t* const this, i2c_address_t const address) {
+	this->address = address;
+	this->pulls[0] = 0x00;
+	this->pulls[1] = 0x00;
+	this->modes[0] = 0xff;
+	this->modes[1] = 0xff;
+	this->outputs[0] = 0x00;
+	this->outputs[1] = 0x00;
 }
 
-bool mcp23017_write(i2c_address_t const address, mcp23017_pin_t const pin, bool const value) {
-	return register_set_pin_bit(address, REG_OUTPUT_LATCH, REG_GPIO, pin, value);
-}
-
-bool mcp23017_write_all(i2c_address_t const address, u16 const value) {
+bool mcp23017_set_mode(mcp23017_device_t* const this, mcp23017_pin_t const pin, mcp23017_mode_t const mode) {
 	io_delay();
-	// rely on auto-increment to write to both banks
-	u8 const buf[] = { REG_GPIO, value & 0xff, value >> 8 };
-	return i2c_send(address, buf, sizeof(buf));
+	return write_reg(this->address, REG_PIN_MODE, this->modes, pin, (bool)mode);
 }
 
-bool mcp23017_read(i2c_address_t const address, mcp23017_pin_t const pin, bool* const ret) {
-	return register_get_pin_bit(address, REG_GPIO, pin, ret);
-}
-
-bool mcp23017_read_all(i2c_address_t address, u16* ret) {
+bool mcp23017_write(mcp23017_device_t* const this, mcp23017_pin_t const pin, bool const value) {
 	io_delay();
-	// rely on auto-increment to read from both banks
-	u8 const reg = REG_GPIO;
-	TRY(i2c_send(address, &reg, sizeof(reg)))
-	return i2c_recv(address, (u8*)ret, sizeof(*ret));
+	return write_reg(this->address, REG_GPIO, this->outputs, pin, value);
 }
 
-bool mcp23017_set_pull(i2c_address_t const address, mcp23017_pin_t const pin, mcp23017_pull_t const pull) {
-	return register_set_pin_bit(address, REG_PULL_UP, REG_PULL_UP, pin, (bool)pull);
+bool mcp23017_read(mcp23017_device_t* const this, mcp23017_pin_t const pin, bool* const ret) {
+	io_delay();
+	return read_reg(this->address, REG_GPIO, pin, ret);
+}
+
+bool mcp23017_read_all(mcp23017_device_t* const this, u16* const ret) {
+	io_delay();
+	return read_u16(this->address, REG_GPIO, ret);
+}
+
+bool mcp23017_set_pull(mcp23017_device_t* const this, mcp23017_pin_t const pin, mcp23017_pull_t const pull) {
+	io_delay();
+	return write_reg(this->address, REG_PULL_UP, this->pulls, pin, (bool)pull);
 }
