8354a41bb2dbb47a3adbbf95eb446652c7f14cd7
Matt Fellenz <matt@felle.nz>
2023-02-23 13:15:37 -0800

    Restart UART terminal on failure

--- uart.sh
+++ uart.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-picocom /dev/ttyUSB0 --baud 115200 --flow n --parity n --databits 8 --stopbits 1 --echo
+while true; do
+	picocom /dev/ttyUSB0 --baud 115200 --flow n --parity n --databits 8 --stopbits 1 --echo && break
+	# continue if error, after waiting a bit
+	sleep 1
+done
