88a716a93c98126d1531e2b9a5da19923cf8d7f5
Matt Fellenz <matt@felle.nz>
2023-03-09 17:48:34 -0800

    Use MMIO struct for mailbox

--- src/mailbox.c
+++ src/mailbox.c
@@ -5,16 +5,18 @@
 
 u32 volatile __attribute__((aligned(16))) mailbox[64];
 
-static u32 volatile* const VIDEOCORE_MAILBOX = (u32 volatile*)(PERIPHERAL_BASE + 0xb880);
-enum {
-	MAILBOX_READ = 0,
-	MAILBOX_STATUS = 6,
-	MAILBOX_CONFIG = 7,
-	MAILBOX_WRITE = 8,
+static struct {
+	u32 read;
+	u32 _res0[5];
+	u32 status;
+	u32 config;
+	u32 write;
+} volatile* const VIDEOCORE_MAILBOX = (void volatile*)(PERIPHERAL_BASE + 0xb880);
 
-	MAILBOX_RESPONSE = 0x8000'0000,
-	MAILBOX_FULL = 0x8000'0000,
-	MAILBOX_EMPTY = 0x4000'0000,
+enum {
+	MAILBOX_RESPONSE = 1u << 31,
+	STATUS_WRITE_FULL = 1 << 31,
+	STATUS_READ_EMPTY = 1 << 30,
 };
 
 bool mailbox_call(mailbox_channel_t const channel) {
@@ -22,34 +24,22 @@ bool mailbox_call(mailbox_channel_t const channel) {
 
 	u32 const command = (u32)(usize)&mailbox | (u32)(channel & 0b1111);
 
-	while (VIDEOCORE_MAILBOX[MAILBOX_STATUS] & MAILBOX_FULL) {
+	while (VIDEOCORE_MAILBOX->status & STATUS_WRITE_FULL) {
 		asm volatile("isb");
 	}
 
-	VIDEOCORE_MAILBOX[MAILBOX_WRITE] = command;
+	VIDEOCORE_MAILBOX->write = command;
 
 	while (true) {
-		while (VIDEOCORE_MAILBOX[MAILBOX_STATUS] & MAILBOX_EMPTY) {
+		while (VIDEOCORE_MAILBOX->status & STATUS_READ_EMPTY) {
 			asm volatile("isb");
 		}
 
-		if (VIDEOCORE_MAILBOX[MAILBOX_READ] == command) {
+		if (VIDEOCORE_MAILBOX->read == command) {
 			return mailbox[1] == MAILBOX_RESPONSE;
 		}
 	}
 }
-
-struct mailbox_command_header {
-	u32 id;
-	u32 buffer_length;
-	u32 value_length;
-};
-
-struct mailbox_command_clock {
-	struct mailbox_command_header header;
-	u32 clock_id;
-	u32 out_rate;
-};
 
 bool mailbox_get_clock_rate(mailbox_clock_t const clock, u32* const ret) {
 	LOG_DEBUG("getting rate of clock %u", clock);
