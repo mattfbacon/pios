e8a45c60300eb5b8d2d7db8ca6d6461e84b5d0fb
Matt Fellenz <matt@felle.nz>
2023-03-10 12:56:54 -0800

    Add calloc and realloc to allocator

--- include/malloc.h
+++ include/malloc.h
@@ -1,5 +1,8 @@
 #pragma once
 
 void malloc_init(void);
+
 void* malloc(usize size);
 void free(void* address);
+void* calloc(usize num_members, usize size);
+void* realloc(void* old, usize new_size);
--- src/malloc.c
+++ src/malloc.c
@@ -1,3 +1,5 @@
+// TODO explore using an XOR linked list for the block list so we can merge blocks backward as well.
+
 #include "base.h"
 #include "mailbox.h"
 #include "malloc.h"
@@ -24,6 +26,7 @@ struct block_header {
 #define BLOCK_SIZE(_block) ((_block)->_size & ADDRESS_MASK)
 #define BLOCK_FREE(_block) ((bool)((_block)->_size & FREE))
 #define BLOCK_NEXT(_block) ((struct block_header*)(usize)(_block)->next)
+#define BLOCK_FROM_DATA(_data) ((struct block_header*)((u8*)(_data) - __builtin_offsetof(struct block_header, data)))
 
 static struct block_header* head_block = NULL;
 
@@ -74,17 +77,87 @@ void* malloc(usize size) {
 	return NULL;
 }
 
+static void try_merge(struct block_header* const block) {
+	for (struct block_header* next = BLOCK_NEXT(block); next && BLOCK_FREE(next); next = BLOCK_NEXT(next)) {
+		block->_size += BLOCK_SIZE(next) + sizeof(struct block_header);
+		block->next = next->next;
+	}
+}
+
 void free(void* address) {
 	if (address == NULL) {
 		return;
 	}
 
-	struct block_header* const block = (struct block_header*)((u8*)address - __builtin_offsetof(struct block_header, data));
+	struct block_header* const block = BLOCK_FROM_DATA(address);
 	block->_size |= FREE;
 
-	// Merge blocks.
-	for (struct block_header* next = BLOCK_NEXT(block); next && BLOCK_FREE(next); next = BLOCK_NEXT(next)) {
-		block->_size += BLOCK_SIZE(next) + sizeof(struct block_header);
-		block->next = next->next;
+	try_merge(block);
+}
+
+void* calloc(usize const num_members, usize const member_size) {
+	if (num_members == 0 || member_size == 0) {
+		return NULL;
+	}
+
+	usize const size = num_members * member_size;
+
+	// A standard way to check for overflow. Optimizes well.
+	if (size / num_members != member_size) {
+		return NULL;
+	}
+
+	u8* const ret = malloc(size);
+	if (ret != NULL) {
+		for (usize i = 0; i < size; ++i) {
+			ret[i] = 0;
+		}
+	}
+	return ret;
+}
+
+void* realloc(void* const old, usize new_size) {
+	if (old == NULL) {
+		return malloc(new_size);
+	} else if (new_size == 0) {
+		if (old != NULL) {
+			free(old);
+		}
+		return NULL;
+	}
+	// old != NULL, new_size != 0
+
+	new_size = align_to(new_size, BLOCK_ALIGNMENT);
+
+	struct block_header* const block = BLOCK_FROM_DATA(old);
+	// We can merge free blocks ahead of this used block into this block, to possibly give us more capacity. This may merge more than it has to, but then we can just split again.
+	try_merge(block);
+	if (BLOCK_SIZE(block) >= new_size + sizeof(struct block_header) + SPLIT_MARGIN) {
+		// Split.
+		struct block_header* const new_next = (struct block_header*)(block->data + new_size);
+		new_next->next = block->next;
+		new_next->_size = (BLOCK_SIZE(block) - new_size - sizeof(struct block_header)) | FREE;
+		try_merge(new_next);
+		block->next = (u32)(usize)new_next;
+		// Clears the free flag.
+		block->_size = new_size;
+		return old;
+	} else if (BLOCK_SIZE(block) >= new_size) {
+		// Keep the old block without splitting.
+		return old;
+	} else {
+		// Make a new allocation and copy.
+		u8* const new_raw = malloc(new_size);
+
+		if (new_raw != NULL) {
+			usize const old_size = BLOCK_SIZE(block);
+			for (usize i = 0; i < old_size; ++i) {
+				new_raw[i] = ((u8*)old)[i];
+			}
+		}
+
+		free(old);
+
+		return new_raw;
 	}
 }
