7dae40d53f647b9af06307eee5afce83e9499638
Matt Fellenz <matt@felle.nz>
2023-03-09 18:17:19 -0800

    Use MMIO struct for PWM

--- src/pwm.c
+++ src/pwm.c
@@ -3,26 +3,31 @@
 #include "mailbox.h"
 #include "pwm.h"
 
-static u32 volatile* const PWM0_BASE = (u32 volatile*)(PERIPHERAL_BASE + 0x20c000);
-static u32 volatile* const PWM1_BASE = (u32 volatile*)(PERIPHERAL_BASE + 0x20c800);
-
-enum {
-	CONTROL = 0,
-	STATUS = 1,
-	CHANNEL0_RANGE = 4,
-	CHANNEL0_DATA = 5,
-	FIFO = 6,
-	CHANNEL1_RANGE = 8,
-	CHANNEL1_DATA = 9,
-
-	CONTROL_MASK = 0xff,
-	CLEAR_FIFO = 1 << 6,
-
-	FIFO_FULL = 1,
-	ERROR_MASK = 0b100111100,
+struct pwm_regs {
+	u32 control;
+	u32 status;
+	u32 dma;
+	u32 _res0;
+	u32 range0;
+	u32 data0;
+	u32 fifo;
+	u32 _res1;
+	u32 range1;
+	u32 data1;
 };
 
-static u32 volatile* controller_base(pwm_controller_t const controller) {
+static struct pwm_regs volatile* const PWM0_BASE = (struct pwm_regs volatile*)(PERIPHERAL_BASE + 0x20c000);
+static struct pwm_regs volatile* const PWM1_BASE = (struct pwm_regs volatile*)(PERIPHERAL_BASE + 0x20c800);
+
+enum {
+	CONTROL_MASK = 0xff,
+	CONTROL_CLEAR_FIFO = 1 << 6,
+
+	STATUS_FIFO_FULL = 1,
+	STATUS_ERROR_MASK = 0b100111100,
+};
+
+static struct pwm_regs volatile* controller_base(pwm_controller_t const controller) {
 	switch (controller) {
 		case pwm_controller_0:
 			return PWM0_BASE;
@@ -48,7 +53,7 @@ void pwm_init_clock(u32 const rate) {
 void pwm_init_channel(pwm_controller_t const controller, pwm_channel_t const channel, pwm_channel_init_flags_t const flags) {
 	LOG_DEBUG("initializing PWM%u channel %u with flags %x", controller, channel, flags);
 	u32 const shift = channel_shift(channel);
-	u32 volatile* const reg = &controller_base(controller)[CONTROL];
+	u32 volatile* const reg = &controller_base(controller)->control;
 	u32 value = *reg;
 	value &= ~(CONTROL_MASK << shift);
 	value |= flags << shift;
@@ -57,32 +62,34 @@ void pwm_init_channel(pwm_controller_t const controller, pwm_channel_t const cha
 
 void pwm_fifo_clear(pwm_controller_t const controller) {
 	LOG_TRACE("clearing PWM%u FIFO", controller);
-	controller_base(controller)[CONTROL] |= CLEAR_FIFO;
+	controller_base(controller)->control |= CONTROL_CLEAR_FIFO;
 }
 
 void pwm_fifo_write(pwm_controller_t const controller, u32 const value) {
-	u32 volatile* const reg = controller_base(controller);
+	struct pwm_regs volatile* const reg = controller_base(controller);
 
 	// wait for room
-	while (reg[STATUS] & FIFO_FULL) {
+	while (reg->status & STATUS_FIFO_FULL) {
 		asm volatile("isb");
 	}
 
 	// actually write
-	reg[FIFO] = value;
+	reg->fifo = value;
 
 	// acknowledge any errors
-	if (reg[STATUS] & ERROR_MASK) {
-		reg[STATUS] = ERROR_MASK;
-	}
+	reg->status = STATUS_ERROR_MASK;
 }
 
 void pwm_set_range(pwm_controller_t const controller, pwm_channel_t const channel, u32 const range) {
 	LOG_DEBUG("setting PWM%u channel %u range to %u", controller, channel, range);
-	controller_base(controller)[CHANNEL0_RANGE + (channel * (CHANNEL1_RANGE - CHANNEL0_RANGE))] = range;
+	struct pwm_regs volatile* const base = controller_base(controller);
+	u32 volatile* const reg = channel ? &base->range1 : &base->range0;
+	*reg = range;
 }
 
 void pwm_set_data(pwm_controller_t const controller, pwm_channel_t const channel, u32 const data) {
 	LOG_TRACE("setting PWM%u channel %u data to %u", controller, channel, data);
-	controller_base(controller)[CHANNEL0_DATA + (channel * (CHANNEL1_DATA - CHANNEL0_DATA))] = data;
+	struct pwm_regs volatile* const base = controller_base(controller);
+	u32 volatile* const reg = channel ? &base->data1 : &base->data0;
+	*reg = data;
 }
