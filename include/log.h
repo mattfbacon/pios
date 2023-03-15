83735e82ccfc466adc0f362cefbc8bdc6d420eb8
Matt Fellenz <matt@felle.nz>
2023-03-15 13:44:27 -0700

    Improve inert expansion of log macros
    The new expansion causes the parameters to be checked at compile-time but not evaluated at runtime.

--- include/log.h
+++ include/log.h
@@ -18,35 +18,36 @@ void log_write(char const* file, u32 line, char const* level, char const* fmt, .
 #if LOG_LEVEL <= LOG_LEVEL_TRACE
 #define LOG_TRACE(...) LOG("trace", ##__VA_ARGS__)
 #else
-#define LOG_TRACE(...)
+// This expansion causes the parameters to be checked at compile-time but not evaluated at runtime.
+#define LOG_TRACE(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_DEBUG
 #define LOG_DEBUG(...) LOG("debug", ##__VA_ARGS__)
 #else
-#define LOG_DEBUG(...)
+#define LOG_DEBUG(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_INFO
 #define LOG_INFO(...) LOG("info", ##__VA_ARGS__)
 #else
-#define LOG_INFO(...)
+#define LOG_INFO(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_WARN
 #define LOG_WARN(...) LOG("warn", ##__VA_ARGS__)
 #else
-#define LOG_WARN(...)
+#define LOG_WARN(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_ERROR
 #define LOG_ERROR(...) LOG("error", ##__VA_ARGS__)
 #else
-#define LOG_ERROR(...)
+#define LOG_ERROR(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_FATAL
 #define LOG_FATAL(...) LOG("fatal", ##__VA_ARGS__)
 #else
-#define LOG_FATAL(...)
+#define LOG_FATAL(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
