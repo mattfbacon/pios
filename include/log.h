556f129245ca218185f766e23fb41c1992e102cb
Matt Fellenz <matt@felle.nz>
2023-03-15 13:50:48 -0700

    Make logs prettier

--- include/log.h
+++ include/log.h
@@ -12,42 +12,42 @@
 #define LOG_LEVEL LOG_LEVEL_INFO
 #endif
 
-void log_write(char const* file, u32 line, char const* level, char const* fmt, ...);
+void log_write(char const* file, u32 line, u32 level, char const* fmt, ...);
 #define LOG(_level, ...) log_write(__FILE__, __LINE__, _level, ##__VA_ARGS__)
 
 #if LOG_LEVEL <= LOG_LEVEL_TRACE
-#define LOG_TRACE(...) LOG("trace", ##__VA_ARGS__)
+#define LOG_TRACE(...) LOG(LOG_LEVEL_TRACE, ##__VA_ARGS__)
 #else
 // This expansion causes the parameters to be checked at compile-time but not evaluated at runtime.
 #define LOG_TRACE(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_DEBUG
-#define LOG_DEBUG(...) LOG("debug", ##__VA_ARGS__)
+#define LOG_DEBUG(...) LOG(LOG_LEVEL_DEBUG, ##__VA_ARGS__)
 #else
 #define LOG_DEBUG(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_INFO
-#define LOG_INFO(...) LOG("info", ##__VA_ARGS__)
+#define LOG_INFO(...) LOG(LOG_LEVEL_INFO, ##__VA_ARGS__)
 #else
 #define LOG_INFO(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_WARN
-#define LOG_WARN(...) LOG("warn", ##__VA_ARGS__)
+#define LOG_WARN(...) LOG(LOG_LEVEL_WARN, ##__VA_ARGS__)
 #else
 #define LOG_WARN(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_ERROR
-#define LOG_ERROR(...) LOG("error", ##__VA_ARGS__)
+#define LOG_ERROR(...) LOG(LOG_LEVEL_ERROR, ##__VA_ARGS__)
 #else
 #define LOG_ERROR(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_FATAL
-#define LOG_FATAL(...) LOG("fatal", ##__VA_ARGS__)
+#define LOG_FATAL(...) LOG(LOG_LEVEL_FATAL, ##__VA_ARGS__)
 #else
 #define LOG_FATAL(...) (0 ? ((void)(__VA_ARGS__)) : ((void)(0)))
 #endif
--- src/log.c
+++ src/log.c
@@ -1,13 +1,16 @@
 #include "log.h"
 #include "uart.h"
 
-void log_write(char const* file, u32 line, char const* level, char const* fmt, ...) {
-	uart_printf("[%s %s:%u] ", level, file, line);
+static char const* const LEVELS[] = { "trace", "debug", "info", "warn", "error", "fatal" };
+static char const* const COLORS[] = { "0;37", "0", "1;30", "1;33", "1;31", "41m\e[1;97" };
+
+void log_write(char const* const file, u32 const line, u32 const level, char const* const fmt, ...) {
+	uart_printf("\e[%sm[%s %s:%u] ", COLORS[level], LEVELS[level], file, line);
 
 	__builtin_va_list args;
 	__builtin_va_start(args, fmt);
 	uart_vprintf(fmt, args);
 	__builtin_va_end(args);
 
-	uart_send_str("\r\n");
+	uart_send_str("\e[0m\r\n");
 }
