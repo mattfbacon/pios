73c50ec0ab29145075373c07d911a97a77d6552c
Matt Fellenz <matt@felle.nz>
2023-03-18 20:01:56 -0700

    Document everything

--- src/main.c
+++ src/main.c
@@ -1,3 +1,23 @@
+#include "malloc.h"
+#include "uart.h"
+
 void main(void) {
-	// your imagination here
+	char* str1 = malloc(16);
+	uart_printf("str1=0x%x\r\n", str1);
+
+	free(str1);
+	uart_send_str("freed\r\n");
+
+	str1 = malloc(16);
+	uart_printf("re1, str1=0x%x\r\n", str1);
+
+	char* str2 = malloc(32);
+	uart_printf("str2=0x%x\r\n", str2);
+
+	free(str1);
+	free(str2);
+	uart_send_str("freed\r\n");
+
+	str1 = malloc(48);
+	uart_printf("re2 str1=0x%x\r\n", str1);
 }
