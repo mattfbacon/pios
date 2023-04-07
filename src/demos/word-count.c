a4f1e239fe903b204f083f2d99aebef3bfa6e9ca
Matt Fellenz <matt@felle.nz>
2023-04-07 16:14:27 -0700

    Fix demo

--- src/demos/word-count.c
+++ src/demos/word-count.c
@@ -2,22 +2,16 @@
 #include "string.h"
 #include "uart.h"
 
-void main(void) {
+[[noreturn]] void main(void) {
 	while (true) {
 		uart_send_str("Enter a line: ");
 
-		u64 count = 0;
+		usize count = 0;
 
 		while (uart_recv() != '\r') {
 			++count;
 		}
 
-		uart_send_str("The line has ");
-		{
-			char buf[21];
-			u64_to_str(buf, count);
-			uart_send_str(buf);
-		}
-		uart_send_str(" bytes.\r\n");
+		uart_printf("The line has %zu bytes\r\n", count);
 	}
 }
