89c4629d59e5f52df2c0c338f69cfb9fbe788ff2
Matt Fellenz <matt@felle.nz>
2023-04-11 14:22:04 -0700

    Reduce I2C logging verbosity
    Honestly, printing the raw data is a trace, not a debug. Make it so.

--- src/i2c.c
+++ src/i2c.c
@@ -40,7 +40,7 @@ void i2c_init(u32 const clock_speed) {
 }
 
 bool i2c_recv(i2c_address_t const address, u8* const buf, u32 const len) {
-	LOG_DEBUG("receiving %u bytes from address %u", len, address);
+	LOG_TRACE("receiving %u bytes from address %u", len, address);
 
 	BASE->peer_address = (u32)address;
 	BASE->control = CONTROL_CLEAR_FIFO;
@@ -73,7 +73,7 @@ bool i2c_recv(i2c_address_t const address, u8* const buf, u32 const len) {
 }
 
 bool i2c_send(i2c_address_t const address, u8 const* const buf, u32 const len) {
-	LOG_DEBUG("sending to address %u the following %u bytes of data: %@d", address, len, buf, len);
+	LOG_TRACE("sending to address %u the following %u bytes of data: %@d", address, len, buf, len);
 
 	BASE->peer_address = (u32)address;
 	BASE->control = CONTROL_CLEAR_FIFO;
